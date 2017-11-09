0e3ce3abce5aa6a6f76cb8bb7315e659e45bbd2e
Mike Tikh <Mike@Mikes-MacBook-Pro.local>
2017-11-09 10:34:48 -0800

    list filter functionality tested

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import '../style/style.css'
 var ViewModel = function() {
   var self = this;
 
+  self.currentInput = ko.observable(0);
+
   self.menuItems = ko.observableArray([]);
 
-  console.log(CASINO_LIST.length)
+  // TODO: Async way to add items to observable array after loading
+  setTimeout(() => {self.menuItems(CASINO_LIST)}, 1000);
+  setTimeout(() => {self.currentInput('pa')}, 2000);
 
-  for (let i = 0; i < CASINO_LIST.length; i++) {
-    console.log('hello')
-    self.menuItems.push(CASINO_LIST[i]);
-  };
+  self.filterItems = ko.computed(() => {
+
+    //TODO: Set different way to check for empty observable
+    if (self.currentInput() == 0) {
+      return self.menuItems();
+    } else {
+      var filter = this.currentInput().toLowerCase();
+
+      return ko.utils.arrayFilter(self.menuItems(), (casino) => {
+        return casino.toLowerCase().indexOf(filter) !== -1;
+      })
+    }
+  })
+
+  // for (let i = 0; i < CASINO_LIST.length; i++) {
+  //   console.log('hello')
+  //   self.menuItems.push(CASINO_LIST[i]);
+  // };
 
   $('#search-button').click(() => {
     if (self.menuItems().length < 13) {
