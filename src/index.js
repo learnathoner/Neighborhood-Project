f970060ff7f99ba001adb3a6e72f1290614e169a
Mike Tikh <Mike@Mikes-MacBook-Pro.local>
2017-11-12 16:50:50 -0800

    knockout in index, working wiki box, labels under icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import '../style/style.css'
+import ko from 'knockout';
+
 
 /* jshint esversion: 6 */
 
@@ -22,6 +24,7 @@ const vegasStrip = {
 };
 // Will store results from places Casino Search
 const CASINO_LIST = [];
+var vm;
 
 /************* MAP FUNCTIONS ************/
 
@@ -75,27 +78,30 @@ function initMap() {
         } else {
             //TODO: Add else if PlacesServiceStatus not OK
         }
-        ko.applyBindings(new ViewModel());
     }
 
     function addCasino(place) {
         CASINO_LIST.push(place.name);
+        vm.menuItems.push(place.name);
     }
 
     // 4. Marker Functions
 
     var defaultIcon = 'https://png.icons8.com/dice/android/40/000000';
     var highlightedIcon = 'https://png.icons8.com/dice/android/40/27ae60';
+    var defaultMarker = makeMarkerIcon(defaultIcon);
+    var highlightedMarker = makeMarkerIcon(highlightedIcon);
 
     // Returns image obj to assign to Marker:icon
     function makeMarkerIcon(iconUrl) {
         let url = `${iconUrl}`;
         var image = {
             url: url,
-            size: new google.maps.Size(21, 34),
+            size: new google.maps.Size(40, 40),
             origin: new google.maps.Point(0, 0),
             anchor: new google.maps.Point(10, 34),
-            scaledSize: new google.maps.Size(21, 34)
+            scaledSize: new google.maps.Size(40, 40),
+            labelOrigin: new google.maps.Point(20, 50)
         };
         return image;
     }
@@ -107,9 +113,10 @@ function initMap() {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
             title: place.name,
+            label: place.name,
             map: map,
             animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
+            icon: defaultMarker,
             position: place.geometry.location
         });
 
@@ -117,19 +124,20 @@ function initMap() {
 
         // On Mouseover changes marker to highlight
         marker.addListener('mouseover', function() {
-            this.setIcon(highlightedIcon);
+            this.setIcon(highlightedMarker);
         });
 
         // On Mouseout, returns marker to default
         marker.addListener('mouseout', function() {
-            this.setIcon(defaultIcon);
+            this.setIcon(defaultMarker);
         });
 
         // OnClick, toggles bounce for marker and sets InfoWindow
         marker.addListener('click', function() {
-            toggleBounce(marker);
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
+          let self = this;
+          toggleBounce(marker);
+
+          getInfoWindowContent(place.name, self);
         });
     }
 
@@ -152,6 +160,95 @@ function initMap() {
 }
 
 /**** MAP FUNCTIONS OUTSIDE initMap ***/
+
+function getInfoWindowContent(casinoName, marker) {
+  // Title of 1st result for wikipedia search
+  retrieveWikiSearch(casinoName)
+    .then((wikiID) => {
+      retrieveWikiObject(wikiID)
+        .then((introPar) => {
+          infowindow.setContent(`
+            <div class="infoWindow">
+              <h3> ${casinoName} </h3>
+              <p> ${introPar} </p>
+            </div>`);
+          infowindow.open(map, marker);
+        })
+    })
+
+  var wikiUrl = `https://en.wikipedia.org/w/api.php?` + $.param({
+    action: 'query',
+    origin: '*',
+    list: 'search',
+    srsearch: `${casinoName}`,
+    format: 'json'
+  });
+
+}
+
+// Retrieves first search result obj for casino name on Wikipedia
+function retrieveWikiSearch(casinoName) {
+
+  // Param for wikipedia search using casino name
+  var searchParam = {
+    action: 'query',
+    origin: '*',
+    list: 'search',
+    srsearch: `${casinoName}`,
+    format: 'json'
+  }
+  // Stores url to fetch search results
+  var wikiUrl = `https://en.wikipedia.org/w/api.php?` + $.param(searchParam);
+
+  // Fetches and returns first result
+  return fetch( wikiUrl, {
+      method: 'POST',
+      headers: new Headers( {
+          'Api-User-Agent': 'somesite.com/whatisthis'
+      } ),
+  } ).then( function ( response ) {
+      if ( response.ok ) {
+        return response.json();
+      }
+      throw new Error( 'Network response was not ok: ' + response.statusText );
+  } ).then( function ( searchResults ) {
+      return searchResults.query.search[0].pageid;
+  });
+}
+
+// Retrieves first search result obj for casino name on Wikipedia
+function retrieveWikiObject(wikiID) {
+
+  // Param to retrieve wiki page
+  var pageParam = {
+    action: 'query',
+    origin: '*',
+    pageids: `${wikiID}`,
+    prop: 'extracts',
+    exintro: '',
+    explaintext: '',
+    rvsection: '0',
+    format: 'json'
+  }
+  // Stores url to fetch search results
+  var wikiUrl = `https://en.wikipedia.org/w/api.php?` + $.param(pageParam);
+
+  // Fetches and returns the wikipedia page
+  return fetch( wikiUrl, {
+      method: 'POST',
+      headers: new Headers( {
+          'Api-User-Agent': 'somesite.com/whatisthis'
+      } ),
+  } ).then( function ( response ) {
+      if ( response.ok ) {
+        return response.json();
+      }
+      throw new Error( 'Network response was not ok: ' + response.statusText );
+  } ).then((wikiPageObject) => {
+      let pageIntroPar = wikiPageObject.query.pages[wikiID].extract;
+      return pageIntroPar;
+  })
+}
 
 // Clears selected when input entered
 function clearSelected() {
@@ -190,7 +287,9 @@ var ViewModel = function() {
     self.currentInput = ko.observable('');
     // Creates observable array list of casinos
     self.menuItems = ko.observableArray([]);
-    self.menuItems(CASINO_LIST);
+
+    //TODO: Erase
+    // self.menuItems(CASINO_LIST);
 
     // Creates filtered array of results when text input changed
     self.filterItems = ko.computed(() => {
@@ -222,5 +321,7 @@ var ViewModel = function() {
         closeInfoWindow();
         updateMarkers(newList);
     });
-
 };
+
+vm = new ViewModel();
+ko.applyBindings(vm);
