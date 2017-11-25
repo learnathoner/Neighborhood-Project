3df235ef5aa41a1843ff8721fd174b8946fd0dba
Mike Tikh <Mike@Mikes-MBP.home>
2017-11-25 00:04:51 -0800

    updated error handling

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ let selectedMarker = '';
 var infowindow;
 // Address used
 const vegasStrip = {
-    lat: 36.1116423,
-    lng: -115.1697258
+  lat: 36.1116423,
+  lng: -115.1697258
 };
 // Variable to hold knockout Viewmodel, allowing external access
 var vm;
@@ -25,9 +25,7 @@ function displayError(error, location = 'map') {
       </p`)
     $('#map').toggleClass('error');
   } else {
-    if (location === 'infoWindow') {
-
-    }
+    if (location === 'infoWindow') {}
   }
 
 }
@@ -43,137 +41,144 @@ var googleMapsAPIOptions = {
 };
 
 // Loads Google Maps then launches initMap as callback
-loadGoogleMapsAPI(googleMapsAPIOptions)
-  .then(function(googleMaps) {
-    initMap();
-  }).catch(function(err) {
-    console.error(`Map could not load: ` + err);
-    let errorMsg = 'Could not load Google Maps: Please check your connection or search parameters!'
-    displayError(errorMsg)
-  });
+loadGoogleMapsAPI(googleMapsAPIOptions).then(function(googleMaps) {
+  initMap();
+}).catch(function(err) {
+  console.error(`Map could not load: ` + err);
+  let errorMsg = 'Could not load Google Maps: Please check your connection or search parameters!'
+  displayError(errorMsg)
+});
 
 /************* MAP FUNCTIONS ************/
 
 function initMap() {
 
-    // 1. Styling
-    // Turns off all labels aside from road names
+  // 1. Styling
+  // Turns off all labels aside from road names
 
-    var myStyle = [{
-        featureType: "administrative",
-        elementType: "labels",
-        stylers: [{
-            visibility: "off"
-        }]
+  var myStyle = [
+    {
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off"
+        }
+      ]
     }, {
-        featureType: "poi",
-        elementType: "labels",
-        stylers: [{
-            visibility: "off"
-        }]
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off"
+        }
+      ]
     }, {
-        featureType: "transit",
-        elementType: "labels",
-        stylers: [{
-            visibility: "off"
-        }]
-      }];
+      featureType: "transit",
+      elementType: "labels",
+      stylers: [
+        {
+          visibility: "off"
+        }
+      ]
+    }
+  ];
 
-    // 2. Map and infowindow creation
+  // 2. Map and infowindow creation
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: vegasStrip,
-        zoom: 15,
-        styles: myStyle
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: vegasStrip,
+    zoom: 15,
+    styles: myStyle
+  });
+
+  infowindow = new google.maps.InfoWindow();
+
+  // 3. Search for Places and initiate Knockout
+
+  // Initiate places service
+  var service = new google.maps.places.PlacesService(map);
+
+  // Search for casinos then run callback
+  service.nearbySearch({
+    location: vegasStrip,
+    radius: 2000,
+    type: ['casino']
+  }, callback);
+
+  // For casino in results - add to KO observable list, create marker
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        addCasino(results[i]);
+        createMarker(results[i]);
+      }
+    } else {
+      console.log('Error: No Casinos found in given location');
+      let errorMsg = 'Google Maps could not find any Casinos in the area!';
+      displayError(errorMsg);
+    }
+  }
+
+  function addCasino(place) {
+    vm.menuItems.push(place.name);
+  }
+
+  // 4. Marker Functions
+
+  // Defines marker icons, turns into image obj for map
+  var defaultIcon = 'https://png.icons8.com/dice/android/40/000000';
+  var highlightedIcon = 'https://png.icons8.com/dice/android/40/27ae60';
+  var defaultMarker = makeMarkerIcon(defaultIcon);
+  var highlightedMarker = makeMarkerIcon(highlightedIcon);
+
+  // Returns image obj to assign to Marker:icon
+  function makeMarkerIcon(iconUrl) {
+    let url = `${iconUrl}`;
+    var image = {
+      url: url,
+      size: new google.maps.Size(40, 40),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(10, 34),
+      scaledSize: new google.maps.Size(40, 40),
+      labelOrigin: new google.maps.Point(20, 50)
+    };
+    return image;
+  }
+
+  // Run as callback on each casion after Places search
+  // Creates initial markers
+  // Pushes each into array and adds listeners (mouseover, click)
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      title: place.name,
+      label: {
+        text: place.name,
+        color: "black"
+      },
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: defaultMarker,
+      position: place.geometry.location
     });
 
-    infowindow = new google.maps.InfoWindow();
+    markers.push(marker);
 
-    // 3. Search for Places and initiate Knockout
+    // On Mouseover changes marker to highlight
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedMarker);
+    });
 
-    // Initiate places service
-    var service = new google.maps.places.PlacesService(map);
+    // On Mouseout, returns marker to default
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultMarker);
+    });
 
-    // Search for casinos then run callback
-    service.nearbySearch({
-        location: vegasStrip,
-        radius: 2000,
-        type: ['casino']
-    }, callback);
-
-    // For casino in results - add to KO observable list, create marker
-    function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                addCasino(results[i]);
-                createMarker(results[i]);
-            }
-        } else {
-            console.log('Error: No Casinos found in given location');
-            let errorMsg = 'Google Maps could not find any Casinos in the area!';
-            displayError(errorMsg);
-        }
-    }
-
-    function addCasino(place) {
-        vm.menuItems.push(place.name);
-    }
-
-    // 4. Marker Functions
-
-    // Defines marker icons, turns into image obj for map
-    var defaultIcon = 'https://png.icons8.com/dice/android/40/000000';
-    var highlightedIcon = 'https://png.icons8.com/dice/android/40/27ae60';
-    var defaultMarker = makeMarkerIcon(defaultIcon);
-    var highlightedMarker = makeMarkerIcon(highlightedIcon);
-
-    // Returns image obj to assign to Marker:icon
-    function makeMarkerIcon(iconUrl) {
-        let url = `${iconUrl}`;
-        var image = {
-            url: url,
-            size: new google.maps.Size(40, 40),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(10, 34),
-            scaledSize: new google.maps.Size(40, 40),
-            labelOrigin: new google.maps.Point(20, 50)
-        };
-        return image;
-    }
-
-    // Run as callback on each casion after Places search
-    // Creates initial markers
-    // Pushes each into array and adds listeners (mouseover, click)
-    function createMarker(place) {
-        var marker = new google.maps.Marker({
-            title: place.name,
-            label: {
-              text: place.name,
-              color: "black"
-            },
-            map: map,
-            animation: google.maps.Animation.DROP,
-            icon: defaultMarker,
-            position: place.geometry.location
-        });
-
-        markers.push(marker);
-
-        // On Mouseover changes marker to highlight
-        marker.addListener('mouseover', function() {
-            this.setIcon(highlightedMarker);
-        });
-
-        // On Mouseout, returns marker to default
-        marker.addListener('mouseout', function() {
-            this.setIcon(defaultMarker);
-        });
-
-        // OnClick, toggles bounce for marker and sets InfoWindow
-        marker.addListener('click', function() {
-          activateMarker(this);
-        });
-    }
+    // OnClick, toggles bounce for marker and sets InfoWindow
+    marker.addListener('click', function() {
+      activateMarker(this);
+    });
+  }
 }
 
 /****** INFOWINDOW & WIKIPEDIA API FUNCTIONS ******/
@@ -181,20 +186,19 @@ function initMap() {
 // Creates and opens infowindow with WikiPedia intro paragraph
 function getInfoWindowContent(casinoName, marker) {
   // Calls function to turn casino name into wikipedia page id
-  retrieveWikiSearch(casinoName)
-    .then((wikiID) => {
-      // Uses wiki page id to retrieve info paragraph
-      retrieveWikiParagraph(wikiID)
-        // Populates infowindow with name and intro paragraph, then opens
-        .then((introPar) => {
-          infowindow.setContent(`
+  retrieveWikiSearch(casinoName).then((wikiID) => {
+    // Uses wiki page id to retrieve info paragraph
+    retrieveWikiParagraph(wikiID)
+    // Populates infowindow with name and intro paragraph, then opens
+      .then((introPar) => {
+      infowindow.setContent(`
             <div class="infoWindow">
               <h3> ${casinoName} </h3>
               <p> ${introPar} </p>
             </div>`);
-          infowindow.open(map, marker);
-        });
+      infowindow.open(map, marker);
     });
+  });
 }
 
 // Searches wikipedia for Casino name, returns first result's PageID
@@ -213,21 +217,21 @@ function retrieveWikiSearch(casinoName) {
   var wikiUrl = `https://en.wikipedia.org/w/api.php?` + $.param(searchParam);
 
   // Fetches and returns first result
-  return fetch( wikiUrl, {
-      method: 'POST',
-      headers: new Headers( {
-          'Api-User-Agent': 'somesite.com/whatisthis'
-      } ),
-  } ).then( function ( response ) {
-      if ( response.ok ) {
-        return response.json();
-      }
-      throw new Error( 'Network response was not ok: ' + response.statusText );
-  } ).catch(function(error) {
-    console.log('There has been a problem with your fetch operation: ' + error.message);
-    alert('Wikipedia Error! Please check your connection or search parameters.')
-  }).then( function ( searchResults ) {
-      return searchResults.query.search[0].pageid;
+  return fetch(wikiUrl, {
+    method: 'POST',
+    headers: new Headers({'Api-User-Agent': 'somesite.com/whatisthis'})
+  // Checks for response from wikipedia, else error
+  }).then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('No response from Wikipedia, please check your connection!')
+    throw new Error('Network response was not ok: ' + response.statusText);
+  // Sends response json to retrieve paragraph, or if unavailable throws error
+  }).then(function(searchResults) {
+    return searchResults.query.search[0].pageid;
+  }).catch(function(err) {
+    alert('Wikipedia search error! Please check your search parameters.')
   });
 }
 
@@ -250,23 +254,20 @@ function retrieveWikiParagraph(wikiID) {
   var wikiUrl = `https://en.wikipedia.org/w/api.php?` + $.param(pageParam);
 
   // Fetches page, if found, returns introparagraph
-  return fetch( wikiUrl, {
-      method: 'POST',
-      headers: new Headers( {
-          'Api-User-Agent': 'somesite.com/whatisthis'
-      } ),
-  } ).then( function ( response ) {
-      if ( response.ok ) {
-        return response.json();
-      }
-      throw new Error( 'Network response was not ok: ' + response.statusText );
-  } ).catch(function(error) {
-    console.log('There has been a problem with your fetch operation: ' + error.message);
-    alert(`Could not find Wikipedia intro paragraph for entry.
-      Please verify that the entry name is correct.`)
+  return fetch(wikiUrl, {
+    method: 'POST',
+    headers: new Headers({'Api-User-Agent': 'somesite.com/whatisthis'})
+  }).then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok: ' + response.statusText);
   }).then((wikiPageObject) => {
-      let pageIntroPar = wikiPageObject.query.pages[wikiID].extract;
-      return pageIntroPar;
+    let pageIntroPar = wikiPageObject.query.pages[wikiID].extract;
+    return pageIntroPar;
+  }).catch(function(error) {
+    alert(`Could not find Wikipedia intro paragraph for entry.
+      Please verify that the entry name and search parameters are correct.`)
   });
 }
 
@@ -274,17 +275,17 @@ function retrieveWikiParagraph(wikiID) {
 
 // Clears selected markers
 function clearSelected() {
-    if (selectedMarker) {
-        selectedMarker.setAnimation(null);
-        selectedMarker = '';
-    }
+  if (selectedMarker) {
+    selectedMarker.setAnimation(null);
+    selectedMarker = '';
+  }
 }
 
 // Closes infoWindow
 function closeInfoWindow() {
-    if (infowindow) {
-        infowindow.close();
-    }
+  if (infowindow) {
+    infowindow.close();
+  }
 }
 
 function activateMarker(marker) {
@@ -294,34 +295,34 @@ function activateMarker(marker) {
 
 // Toggle Bounce animation for markers
 function toggleBounce(marker) {
-    // If clicking on the same marker, turn bounce off
-    if (marker.animation) {
-        marker.setAnimation(null);
-    } else {
-        // If a marker is currently selected, turn off its animation
-        if (selectedMarker) {
-            selectedMarker.setAnimation(null);
-        }
-        // Set bounce animation on new marker
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        // Sets new marker as currently selected
-        selectedMarker = marker;
+  // If clicking on the same marker, turn bounce off
+  if (marker.animation) {
+    marker.setAnimation(null);
+  } else {
+    // If a marker is currently selected, turn off its animation
+    if (selectedMarker) {
+      selectedMarker.setAnimation(null);
     }
+    // Set bounce animation on new marker
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    // Sets new marker as currently selected
+    selectedMarker = marker;
+  }
 }
 
 // Called when search-box input changed
 // Filters visible markers based on new input
 function updateMarkers(filteredList) {
-    // For each marker:
-    // If marker in filteredList, visible=true, otherwise hidden
-    // filteredList is an array of markers that match the current input
-    for (let marker of markers) {
-        if (filteredList.indexOf(marker.title) != -1) {
-            marker.setVisible(true);
-        } else {
-            marker.setVisible(false);
-        }
+  // For each marker:
+  // If marker in filteredList, visible=true, otherwise hidden
+  // filteredList is an array of markers that match the current input
+  for (let marker of markers) {
+    if (filteredList.indexOf(marker.title) != -1) {
+      marker.setVisible(true);
+    } else {
+      marker.setVisible(false);
     }
+  }
 }
 
 /******* KNOCKOUT VIEWMODEL ********/
@@ -343,20 +344,20 @@ var ViewModel = function() {
     closeInfoWindow();
     clearSelected();
 
-      // If currentInput empty, returns original menuItem list
-      if (self.currentInput() === '') {
-          filteredList = self.menuItems();
-      } else {
-          // If currentInput not empty, creates filtered array based on input
-          var filter = this.currentInput().toLowerCase();
+    // If currentInput empty, returns original menuItem list
+    if (self.currentInput() === '') {
+      filteredList = self.menuItems();
+    } else {
+      // If currentInput not empty, creates filtered array based on input
+      var filter = this.currentInput().toLowerCase();
 
-          filteredList = ko.utils.arrayFilter(self.menuItems(), (casino) => {
-              return casino.toLowerCase().indexOf(filter) !== -1;
-            });
-      }
-      // Calls function to update Markers with new list, returns new list
-      updateMarkers(filteredList);
-      return filteredList;
+      filteredList = ko.utils.arrayFilter(self.menuItems(), (casino) => {
+        return casino.toLowerCase().indexOf(filter) !== -1;
+      });
+    }
+    // Calls function to update Markers with new list, returns new list
+    updateMarkers(filteredList);
+    return filteredList;
   });
 
   self.selectMarker = function(casino, event) {
